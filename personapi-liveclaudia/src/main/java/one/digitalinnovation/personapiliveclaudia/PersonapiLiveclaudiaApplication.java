11babc70af0e751896debbc276735034ef330014
Claudia Egas Terra <49598466+egasterra@users.noreply.github.com>
2021-09-03 15:31:55 -0300

    Add files via upload

---
+++ personapi-liveclaudia/src/main/java/one/digitalinnovation/personapiliveclaudia/PersonapiLiveclaudiaApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.personapiliveclaudia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonapiLiveclaudiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonapiLiveclaudiaApplication.class, args);
+	}
+
+}
---
+++ personapi-liveclaudia/src/test/java/one/digitalinnovation/personapiliveclaudia/PersonapiLiveclaudiaApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.personapiliveclaudia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiLiveclaudiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
